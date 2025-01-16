a3c84c9906b640b02e75dd0a82140c52a2096389
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-16 11:02:55 +0530

    Conditional Statements

---
+++ JavaScript/56Day/index.js
@@ -0,0 +1,14 @@
+console.log("Learning Conditional Statements in JavaScript");
+
+let age = 21;
+
+if (age > 18) {
+  console.log("You're an adult");
+}
+
+let adult = 32;
+if (adult >= 19) {
+  console.log("Now, You're eligible to drive");
+} else {
+  console.log("No, You're not eligible to drive");
+}
