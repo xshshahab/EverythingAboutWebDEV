ae0fb75929b30ebd87d12d7620619d44ebc1451c
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-17 22:54:35 +0530

    Document Object Modal in JS

--- JavaScript/66Day/index.js
+++ JavaScript/66Day/index.js
@@ -3,3 +3,10 @@ const heading = document.getElementById("main-heading");
 
 // Change its text content
 heading.textContent = "Syphar Learning JS";
+
+document.title = "Syphar | Learning JavaScript";
+document.body.style.background = "linear-gradient(to bottom, #f7f7f7, #f7f7f7)";
+
+console.log(document.title);
+console.log(document.body);
+console.log(document.body.style.background);
