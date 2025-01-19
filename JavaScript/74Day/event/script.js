859d1c5c5df7da300fb4e84468f54e994bfef3e3
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-19 11:27:32 +0530

    Event Bubbling, event Propagation in JS

---
+++ JavaScript/74Day/event/script.js
@@ -0,0 +1,11 @@
+document.querySelector(".child").addEventListener("click", () => {
+  alert("Child Was clicked");
+});
+
+document.querySelector(".child-container").addEventListener("click", () => {
+  alert("Child container Was clicked");
+});
+
+document.querySelector(".container").addEventListener("click", () => {
+  alert("container Was clicked");
+});
