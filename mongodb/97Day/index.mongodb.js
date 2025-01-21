d4d9b5fd474d6359b6eb9df938fa626fd76a5027
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-21 22:34:10 +0530

    Mongoose and Express Challenge

---
+++ mongodb/97Day/index.mongodb.js
@@ -0,0 +1,13 @@
+// Generate a dummy data in this format in a collection called employees in a db called company
+
+// {
+//     name: "Syphar",
+//     salary: 504000,
+//     language: "Java",
+//     city: "Las Vegas",
+//     isManager: false
+// }
+
+// Generate 10 such records when a button called generate data is clicked!
+// Create an Express app with mongoose to acheive it
+// Everytime the button is clicked, you should clear the collection
