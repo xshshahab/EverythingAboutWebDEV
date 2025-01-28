91d44fe5fb9819f540de037c128ffce458e84c67
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-28 19:42:16 +0530

    Project setup

---
+++ projects/password-manager/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Manager from './components/Manager/Manager'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Manager />
+      <Footer />
+    </>
+  )
+}
+
+export default App
