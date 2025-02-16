36da5b0b4b742416cd847bf0d3786511bb797f8f
xshshahab <mdshahbuddin9504@gmail.com>
2025-02-16 20:46:08 +0530

    TYpes understanding

---
+++ TypeScript/Types/function.js
@@ -0,0 +1,12 @@
+function abcd() {
+    console.log("ABCD function");
+}
+function numb() {
+    return 12;
+}
+function str() {
+    return "String return karunga";
+}
+function bool() {
+    return false;
+}
---
+++ TypeScript/Types/function.ts
@@ -0,0 +1,15 @@
+function abcd(): void {
+  console.log("ABCD function");
+}
+
+function numb(): number {
+  return 12;
+}
+
+function str(): string {
+  return "String return karunga";
+}
+
+function bool(): boolean {
+  return false;
+}
