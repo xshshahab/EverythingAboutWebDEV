f5b10c91b8ae1e9450cc885b5d1171b17e3b1ad5
xshshahab <mdshahbuddin9504@gmail.com>
2025-02-17 18:29:49 +0530

    Fundamental of Class in TS

---
+++ TypeScript/ClassesAndObject/Classes/app.ts
@@ -0,0 +1,12 @@
+class Device {
+  name = "LG";
+  model = "OLED";
+  price = 15000;
+}
+
+let d1 = new Device();
+console.log(d1.name); // LG
+console.log(d1.model); // OLED
+console.log(d1.price); // 15000
+
+let d2 = new Device();
