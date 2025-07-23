0fe3b17996216d0b1dbc494a36abfbec8674c41b
Md Shahabuddin <mdshahbuddin9504@gmail.com>
2025-07-23 10:31:34 +0530

    Function default and rest

---
+++ XSH_SHAHAB_CODESPACES/core/03_BASICS/index.js
@@ -0,0 +1,5 @@
+function add(v1 = 0, v2 = 0) {
+  console.log(v1 + v2);
+}
+
+add(1, 22);
---
+++ XSH_SHAHAB_CODESPACES/core/03_BASICS/script.js
@@ -0,0 +1,5 @@
+function restFn(...rest) {
+  console.log("Rest Fn: ", rest);
+}
+
+restFn(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
