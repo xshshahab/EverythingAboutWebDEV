aff507435c4413eac02932c2a4c1e6ad35dac39e
Md Shahabuddin <mdshahbuddin9504@gmail.com>
2025-07-31 21:57:00 +0530

    Basic Objects revised

---
+++ #100Days-WEBDEV/JavaScript/Day_03_DOM/section-01/script.js
@@ -0,0 +1,7 @@
+let h1 = document.querySelector("h1");
+
+h1.innerText = "Hey JS Developers!";
+
+h1.textContent = "Hey, JavaScript Dev";
+
+h1.innerHTML = "Hey, Shahab Kaise ho";
---
+++ #100Days-WEBDEV/JavaScript/Day_03_DOM/section-02/script.js
@@ -0,0 +1,11 @@
+let firstH1 = document.getElementById("first-h1");
+console.log(firstH1);
+
+let firstH2 = document.getElementsByClassName("first-h2");
+console.log(firstH2);
+
+let firstH1WithQuery = document.querySelector("h1");
+console.dir(firstH1WithQuery);
+
+let selectAllH2 = document.querySelectorAll("h2");
+console.dir(selectAllH2);
